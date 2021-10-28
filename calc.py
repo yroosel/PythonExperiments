2d65fcc8a79233d15d56e1afaf4cf3ec4f37f674
yroosel <yvan.rooseleer@biasc.be>
2021-10-28 11:47:29 +0200

    Changed unittest example (calc)

--- calc.py
+++ calc.py
@@ -8,10 +8,16 @@ def multiply(n1, n2):
     return n1*n2
 
 def divide(n1, n2):
-    return n1/n2
+    if n2 != 0:
+        return n1/n2
+    else:
+        return ("Error: Division by Zero")
 
 def main():
     print (add(100,10))
+    print (subtract(100,10))
+    print (multiply(100,10))
+    print (divide(100,10))
 
 if __name__ == "__main__":
    main()
