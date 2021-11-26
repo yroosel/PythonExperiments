0abb56569e53cf1b12c5220079d82ca1bc0ce276
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2021-11-26 19:35:31 +0100

    Created salted password code

---
+++ salted_password.py
@@ -0,0 +1,10 @@
+import os
+import hashlib
+salt = os.urandom(32)
+plaintext = 'hellow0rld'.encode()
+
+digest = hashlib.pbkdf2_hmac('sha256', plaintext, salt, 10000)
+hex_hash = digest.hex()
+print(hex_hash)
+byte_hash = digest.fromhex(digest.hex())
+print(byte_hash)
