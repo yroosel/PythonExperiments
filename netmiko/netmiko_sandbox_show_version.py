7d804d837c96e4b9927d0b0509f58b939a1c7ebd
yroosel <yvan.rooseleer@biasc.be>
2022-03-17 13:58:52 +0100

    Updated Netmiko Sandbox URL

--- netmiko/netmiko_sandbox_show_version.py
+++ netmiko/netmiko_sandbox_show_version.py
@@ -5,12 +5,18 @@ print(datetime.datetime.now())
 print("Connecting via SSH => show version")
 #
 from netmiko import ConnectHandler
+### VAR
+RTR="sandbox-iosxe-latest-1.cisco.com"
+SSH_PORT="22"
+USER="developer" 
+PW="C1sco12345"
+### EXEC
 sshCli = ConnectHandler(
     device_type="cisco_ios",
-    host="sandbox-iosxe-recomm-1.cisco.com",
-    port="22",
-    username="developer",
-    password="C1sco12345"
+    host=RTR,
+    port=SSH_PORT,
+    username=USER,
+    password=PW
     )
 output=sshCli.send_command("show version")
 print(output)
