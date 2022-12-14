272b4eda24eda745a6d6fa21ff9eba3e97cf3269
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2022-12-14 17:33:39 +0100

    Update netconf_show_running.py
    Added FILTER

--- netconf/netconf_show_running.py
+++ netconf/netconf_show_running.py
@@ -1,14 +1,25 @@
+from ncclient import manager
 import xml.dom.minidom
 import datetime
 print ("Current date and time: ")
 print(datetime.datetime.now())
-NETCONF_ROUTER_IP = "192.168.56.107"
-NETCONF_SSH_PORT = "830"
-NETCONF_user = input("Username? ")  # ciscocisco
-NETCONF_psw = input("Password? ")   # cisco123!
-from ncclient import manager
 print('Starting NETCONFIG')
 print('Connecting to virtual router with ncclient')
+NETCONF_ROUTER_IP = "192.168.56.101"
+NETCONF_SSH_PORT = "830"
+NETCONF_user = "cisco"
+NETCONF_psw = "cisco123!"
+# FILTER is needed for netconf
+FILTER = """
+<filter>
+  <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+    <interface>
+       <name></name>
+    </interface>
+  </interfaces>
+</filter>
+"""
+# CONNECTING
 m = manager.connect(
         host=NETCONF_ROUTER_IP,
         port=NETCONF_SSH_PORT,
@@ -16,9 +27,10 @@ m = manager.connect(
         password=NETCONF_psw,
         hostkey_verify=False
         )
+# OUTPUTS
 print("Connection Succeeded:", end=" ")
 print(m.connected)
 print('Retrieving get-config from virtual router')
-netconfig_reply = m.get_config(source="running")
+netconfig_reply = m.get_config("running", FILTER)
 print('Parsing and printing XML')
 print(xml.dom.minidom.parseString(netconfig_reply.xml).toprettyxml())
