cef0fad0e5cf8d4241a80bbdaf72713bccd17895
yroosel <yvan.rooseleer@biasc.be>
2023-02-16 14:25:12 +0000

    Added filter and loop

--- json/ip-address-parsing.py
+++ json/ip-address-parsing.py
@@ -5,6 +5,12 @@ multiple_addresses = [{'ip': '192.168.56.101', 'netmask': '255.255.255.0'},
 ietf_ipv4 = {'address': [{'ip': '192.168.56.101', 'netmask': '255.255.255.0'}]}
 
 print(type(single_address))
+print(single_address['ip'])
 print(type(multiple_addresses))
+print(multiple_addresses[0]['ip'])
+print(multiple_addresses[1]['ip'])
 print(type(ietf_ipv4))
 print(ietf_ipv4['address'][0]['ip'])
+#loop
+for addr in multiple_addresses:
+    print(addr)
