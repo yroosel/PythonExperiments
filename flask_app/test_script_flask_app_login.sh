d2b151a02f9788e560247e4159772445d4c6f075
Yvan Rooseleer <yvan@biasc.com>
2023-04-13 18:33:22 +0000

    Updated flask login demo

--- flask_app/test_script_flask_app_login.sh
+++ flask_app/test_script_flask_app_login.sh
@@ -2,15 +2,15 @@
 # programm flask_app_login needs to be running
 echo "Deleting all test records"
 curl -k -X DELETE "https://127.0.0.1:5555/delete/all"
-INSECURE_USER="USER_INSECURE"
-INSECURE_PW="123456"
+INSECURE_USER="USER_INSECURE_007"
+INSECURE_PW="123456007"
 echo 'Creating new user v1 insecure'
 curl -k -X POST -F 'username='$INSECURE_USER -F 'password='$INSECURE_PW 'https://127.0.0.1:5555/signup/v1'
 echo "Testing login v1"
 curl -k -X POST -F 'username='$INSECURE_USER -F 'password='$INSECURE_PW 'https://127.0.0.1:5555/login/v1'
 #### Replace fixed username with a variable SECURE_USER and a variable passord SECURE_PW
 echo "Creating new user v2 secure"
-curl -k -X POST -F 'username=usersec' -F 'password=123951' 'https://127.0.0.1:5555/signup/v2'
+curl -k -X POST -F 'username=USER_SEC_007' -F 'password=123951007' 'https://127.0.0.1:5555/signup/v2'
 echo "Testing login v2"
-curl -k -X POST -F 'username=usersec' -F 'password=123951' 'https://127.0.0.1:5555/login/v2'
+curl -k -X POST -F 'username=USER_SEC_007' -F 'password=123951007' 'https://127.0.0.1:5555/login/v2'
 
