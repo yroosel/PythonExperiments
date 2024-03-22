e2c92f24d4e2fe4c353a0cf15d8ea8b14c2a1c3f
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2024-03-22 07:27:11 +0100

    Username and password

--- netmiko/netmiko_show_version.py
+++ netmiko/netmiko_show_version.py
@@ -12,8 +12,8 @@ sshCli = ConnectHandler(
     device_type="cisco_ios",
     host="192.168.56.101",
     port="22",
-    username="cisco",
-    password="cisco123!"
+    username="Enter your username",
+    password="Enter your password"
     )
 output=sshCli.send_command("show version")
 for line in output.splitlines():
