d5c0f431d58b85f21f3be5130ce16b402c2899ba
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2024-12-17 17:11:51 +0100

    Update restconf_interfaces_get.py

--- restconf/restconf_interfaces_get.py
+++ restconf/restconf_interfaces_get.py
@@ -1,3 +1,4 @@
+#### MAKE SURE YOU HAVE RESERVED A IOS XE ROUTER ON DEVNET
 #### RESTCONF LAB 8.3.7
 #### Part 6: Use a Python script to Send GET Requests
 
@@ -7,26 +8,21 @@ import requests
 requests.packages.urllib3.disable_warnings()
 
 #### Step 2: Create the variables that will be the components of the request
-IP_HOST="192.168.56.101"
-RESTCONF_USERNAME="cisco"
-RESTCONF_PASSWORD="cisco123!"
-DATA_FORMAT="application/yang-data+json"
-LOOPBACK_INTERFACE="Loopback11"
-LOOPBACK_IP="10.1.1.11"
+IP_ADDRESS = "10.10.20.48"
+RESTCONF_USERNAME = "developer"
+RESTCONF_PASSWORD = "C1sco12345"
 basicauth = (RESTCONF_USERNAME, RESTCONF_PASSWORD)
-api_url = f"https://{IP_HOST}/restconf/data/ietf-interfaces:interfaces/interface={LOOPBACK_INTERFACE}"
-headers = { "Accept": "application/yang-data+json",  "Content-type":"application/yang-data+json" }
+api_url = f"https://{IP_ADDRESS}/restconf/data/ietf-interfaces:interfaces"
+headers = { "Accept": "application/yang-data+xml",  "Content-type":"application/yang-data+json" }
 
 #### Step 3: Create a variable to send the request and store the JSON response
 resp = requests.get(api_url, auth=basicauth, headers=headers, verify=False)
-print(resp.status_code)
 #print(dir(resp))
-#print(resp.text)
-
+print(resp.status_code)
 #### Step 4: Format and display the JSON data received from the CSR1kv.
-response_json = resp.json()
+#response_json = resp.json()
 #print(response_json)
-print(json.dumps(response_json, indent=4))
-#print(resp.status_code)
-print(json.dumps(response_json, indent=4))
+#print(json.dumps(response_json, indent=4))
+response = resp.text
+print(response)
 
