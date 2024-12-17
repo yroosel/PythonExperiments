a0ff63a95505891e36905ef9c0c4f389ad0526b6
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2024-12-17 16:38:24 +0100

    Update dnac_hello_world_excel_2.py

--- dnac/dnac_hello_world_excel_2.py
+++ dnac/dnac_hello_world_excel_2.py
@@ -8,7 +8,7 @@ requests.packages.urllib3.disable_warnings()
 
 # Constants for DNA Center API access
 DNAC_SCHEME = 'https://'
-DNAC_AUTHORITY = 'sandboxdnac2.cisco.com'
+DNAC_AUTHORITY = 'sandboxdnac.cisco.com'
 DNAC_PORT = ':443'
 DNAC_PATH_TOKEN = '/dna/system/api/v1/auth/token'
 DNAC_PATH_NETWORK_DEVICES = '/dna/intent/api/v1/network-device'
