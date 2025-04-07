42144d8c1440b685fdeae16ffccc6c62982bf4f0
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2025-04-07 10:27:16 +0200

    Update restconf_loopback_put.py

--- restconf/restconf_loopback_put.py
+++ restconf/restconf_loopback_put.py
@@ -5,24 +5,27 @@ import json
 import requests
 requests.packages.urllib3.disable_warnings()
 #### Step 2: Create the variables that will be the components of the request
-IP_HOST="https://sandboxdnac.cisco.com"
-RESTCONF_USERNAME="Enter your username"
-RESTCONF_PASSWORD="Enter your password"
-DATA_FORMAT="application/yang-data+json"
-api_url = f"https://{IP_HOST}/restconf/data/ietf-interfaces:interfaces/interface=Loopback101"
+IP_ADDRESS="192.168.56.10x"  ### "10.10.20.48"
+RESTCONF_USERNAME="Your User" 
+RESTCONF_PASSWORD="Your Pw" 
+
+LOOPBACK_NAME="Loopback113"
+LOOPBACK_IP_ADDR="10.113.10.1"
+
+api_url = f"https://{IP_ADDRESS}/restconf/data/ietf-interfaces:interfaces/interface={LOOPBACK_NAME}"
 headers = { "Accept": "application/yang-data+json",  "Content-type":"application/yang-data+json"  }
 basicauth = (RESTCONF_USERNAME, RESTCONF_PASSWORD)
 
 yangConfig = {
   "ietf-interfaces:interface": {
-     "name": "Loopback101",
-     "description": "RESTCONF loopback 101",
+     "name": LOOPBACK_NAME,
+     "description": "RESTCONF loopback",
      "type": "iana-if-type:softwareLoopback",
      "enabled": True,
      "ietf-ip:ipv4": {
          "address": [
              {
-                 "ip": "10.1.0.1",
+                 "ip": LOOPBACK_IP_ADDR,
                  "netmask": "255.255.255.0"
              }
          ]
@@ -31,10 +34,13 @@ yangConfig = {
  }
 }
 
-#### Step 3: Create a variable to send the request and store the JSON response
-resp = requests.put(api_url, data=json.dumps(yangConfig), auth=basicauth, headers=headers, verify=False)
+#### Step 3: Send the request and store the JSON response
+#### Select required action below:
+ACTION_REQUIRED = "PUT"  ### ACTION_REQUIRED = "DELETE"
+
+resp = requests.request(ACTION_REQUIRED,api_url, data=json.dumps(yangConfig), auth=basicauth, headers=headers, verify=False)
 
 if(resp.status_code >= 200 and resp.status_code <= 299):
  print("STATUS OK: {}".format(resp.status_code))
 else:
- print('Error. Status Code: {} \nError message: {}'.format(resp.status_code,resp.json()))
+ print("Error. Status Code: {} \nError message: {}".format(resp.status_code,resp.json()))
