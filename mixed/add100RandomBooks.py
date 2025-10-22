d8f4d7f8f8b1fceee935eb6f9c869c6d0221b9d1
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2025-10-22 10:49:22 +0200

    Create add100RandomBooks.py

---
+++ mixed/add100RandomBooks.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+from faker import Faker
+
+APIHOST = "http://library.demo.local"
+LOGIN = "Type your username"
+PASSWORD = "Type your password"
+
+def getAuthToken():
+    authCreds = (LOGIN, PASSWORD)
+    r = requests.post(
+        f"{APIHOST}/api/v1/loginViaBasic", 
+        auth = authCreds
+    )
+    if r.status_code == 200:
+        return r.json()["token"]
+    else:
+        raise Exception(f"Status code {r.status_code} and text {r.text}, while trying to Auth.")
+
+def addBook(book, apiKey):
+    r = requests.post(
+        f"{APIHOST}/api/v1/books", 
+        headers = {
+            "Content-type": "application/json",
+            "X-API-Key": apiKey
+            },
+        data = json.dumps(book)
+    )
+    if r.status_code == 200:
+        print(f"Book {book} added.")
+    else:
+        raise Exception(f"Error code {r.status_code} and text {r.text}, while trying to add book {book}.")
+
+# Get the Auth Token Key
+# apiKey = getAuthToken()
+apiKey = "cisco|3PNjBC-OJn3a5XK8VS-swKMDKBtbioP24Xq_5SCBRcI"
+
+# Using the faker module, generate random "fake" books
+fake = Faker()
+for i in range(900,999):
+    fakeTitle = fake.catch_phrase()
+    fakeAuthor = fake.name()
+    fakeISBN = fake.isbn13()
+    book = {"id":i, "title": fakeTitle, "author": fakeAuthor, "isbn": fakeISBN}
+    # add the new random "fake" book using the API
+    addBook(book, apiKey) 
+
