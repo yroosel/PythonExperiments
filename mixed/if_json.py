4f3a296a8dbd4182a0026ce5521e9dd0aff2c692
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2025-11-05 11:47:00 +0100

    Update if_json.py

--- mixed/if_json.py
+++ mixed/if_json.py
@@ -1,17 +1,18 @@
-# Practice json and dict structures, transform into yaml
-import json
-if_json = {"ietf-interfaces:interfaces": 
-        {"interface": [
-            {"name": "GigabitEthernet1", "description": "VBox", "type": "iana-if-type:ethernetCsmacd", "enabled": True, 
-             "ietf-ip:ipv4": 
-             {"address": [{"ip": "192.168.56.101", "netmask": "255.255.255.0"}]}, 
-             "ietf-ip:ipv6": {}}, 
-            {"name": "Loopback9", "description": "999", "type": "iana-if-type:softwareLoopback", "enabled": True, 
-             "ietf-ip:ipv4": 
-             {"address": [{"ip": "10.9.9.9", "netmask": "255.255.255.0"},
-                          {"ip": "172.29.0.9", "netmask": "255.255.255.0"}]}, "ietf-ip:ipv6": {}
-            }
-        ]
-        }
-       }
+if_dict = {"ietf-interfaces:interfaces": 
+           {"interface": [
+               {"name": "GigabitEthernet1", "description": "VBox", "type": "iana-if-type:ethernetCsmacd", "enabled": True, 
+                "ietf-ip:ipv4": {"address": [{"ip": "192.168.56.101", "netmask": "255.255.255.0"}]}, 
+                "ietf-ip:ipv6": {}
+               }, 
+               {"name": "Loopback9", "description": "999", "type": "iana-if-type:softwareLoopback", "enabled": True, 
+                "ietf-ip:ipv4": {"address": [{"ip": "10.9.9.9", "netmask": "255.255.255.0"},
+                                             {"ip": "172.29.0.9", "netmask": "255.255.255.0"}]}, 
+                "ietf-ip:ipv6": {}
+               }
+           ]
+           }
+          }
+print(type(if_dict))
+print(if_dict)
+if_json = json.dumps(if_json, indent=2)
 print(if_json)
