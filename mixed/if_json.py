0233f25718853d7deede9eaac1c1673b89530cfc
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2025-11-05 11:16:35 +0100

    Create if_json.py

---
+++ mixed/if_json.py
@@ -0,0 +1,15 @@
+if_json = {"ietf-interfaces:interfaces": 
+        {"interface": [
+            {"name": "GigabitEthernet1", "description": "VBox", "type": "iana-if-type:ethernetCsmacd", "enabled": True, 
+             "ietf-ip:ipv4": 
+             {"address": [{"ip": "192.168.56.101", "netmask": "255.255.255.0"}]}, 
+             "ietf-ip:ipv6": {}}, 
+            {"name": "Loopback9", "description": "999", "type": "iana-if-type:softwareLoopback", "enabled": True, 
+             "ietf-ip:ipv4": 
+             {"address": [{"ip": "10.9.9.9", "netmask": "255.255.255.0"},
+                          {"ip": "172.29.0.9", "netmask": "255.255.255.0"}]}, "ietf-ip:ipv6": {}
+            }
+        ]
+        }
+       }
+print(if_json)
