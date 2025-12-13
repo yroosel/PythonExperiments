6d741fcfed52338fc6fdf6a215accaae72372826
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2025-12-13 17:37:29 +0100

    Update flask_app_signup.py

--- flask_app/flask_app_signup.py
+++ flask_app/flask_app_signup.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3
+# Check SQL injection vurnerability
 
 app = Flask(__name__)
 
@@ -12,14 +13,19 @@ def signup_v1():
     c = db_conn.cursor()
     if request.method == 'GET':
         return render_template("signup.html")
-    try:
-        c.execute(
-            "INSERT INTO USER_PLAIN (USERNAME, PASSWORD) VALUES (?, ?)",
-            (request.form['username'], request.form['password'])
-        )
-        db_conn.commit()
+    if request.method == 'POST':
+        try:
+            username_entered = request.form.get('username')
+            password_entered = request.form.get('password')
+            if not username_entered or not password_entered:
+                return "Missing username or password", 400
+            c.execute(
+                "INSERT INTO USER_PLAIN (USERNAME, PASSWORD) VALUES (?, ?)",
+                 (username_entered, password_entered)
+                )
+            db_conn.commit()
         return "Signup success\n"
-
+        
     except sqlite3.IntegrityError:
         return "Username already exists\n"
 
