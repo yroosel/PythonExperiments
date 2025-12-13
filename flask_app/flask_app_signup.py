4ae24281b040b38b88bbf787a9371f66ac9e0017
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2025-12-13 17:52:51 +0100

    Update flask_app_signup.py

--- flask_app/flask_app_signup.py
+++ flask_app/flask_app_signup.py
@@ -1,35 +1,40 @@
 from flask import Flask, render_template, request
 import sqlite3
-# Check SQL injection vurnerability
+# Advice: create safer version the hashes passwords
+# Question: Is this code safe against SQL INjection ?
 
 app = Flask(__name__)
 
-# Database needs to exist prior to running this script
 db_name = "user.db"
 
 @app.route('/signup/v1', methods=['GET', 'POST'])
 def signup_v1():
-    db_conn = sqlite3.connect(db_name)
-    c = db_conn.cursor()
+
     if request.method == 'GET':
         return render_template("signup.html")
-    if request.method == 'POST':
-        try:
-            username_entered = request.form.get('username')
-            password_entered = request.form.get('password')
-            if not username_entered or not password_entered:
-                return "Missing username or password", 400
-            c.execute(
-                "INSERT INTO USER_PLAIN (USERNAME, PASSWORD) VALUES (?, ?)",
-                 (username_entered, password_entered)
-                )
-            db_conn.commit()
+
+    # POST request
+    db_conn = sqlite3.connect(db_name)
+    c = db_conn.cursor()
+
+    try:
+        username_entered = request.form.get('username')
+        password_entered = request.form.get('password')
+
+        if not username_entered or not password_entered:
+            return "Missing username or password", 400
+
+        c.execute(
+            "INSERT INTO USER_PLAIN (USERNAME, PASSWORD) VALUES (?, ?)",
+            (username_entered, password_entered)
+        )
+        db_conn.commit()
         return "Signup success\n"
-        
+
     except sqlite3.IntegrityError:
         return "Username already exists\n"
 
     finally:
         db_conn.close()
 
-app.run()
+app.run(debug=True)
