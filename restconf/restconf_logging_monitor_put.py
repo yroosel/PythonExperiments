a3c641661615dccfe7235b3118826cabb8f1e827
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2026-02-06 14:16:21 +0100

    Update restconf_logging_monitor_put.py

--- restconf/restconf_logging_monitor_put.py
+++ restconf/restconf_logging_monitor_put.py
@@ -1,15 +1,12 @@
-import json
+# Configure logging severity
 import requests
 requests.packages.urllib3.disable_warnings()
-IP_ADDRESS = "Get IP Address"
-api_url = f"https://{IP_ADDRESS}/restconf/data/Cisco-IOS-XE-native:native/logging/monitor/severity"
-
-headers = {"Accept":"application/yang-data+json",
-           "Content-type":"application/yang-data+json"
-           }
-username = "Get Username"
-password = "Get Password"
-basicauth = (username, password)
-warconf = {"severity": "warnings"}
-resp = requests.put(api_url, data=json.dumps(warconf), auth=basicauth, headers=headers, verify=False)
-print(resp)
+IP_ADDRESS="Get IP Address"
+RESTCONF_USERNAME= "Get Username"
+RESTCONF_PASSWORD="Get Password"
+url = f"https://{IP_ADDRESS}/restconf/data/Cisco-IOS-XE-native:native/logging"
+auth = (RESTCONF_USERNAME,RESTCONF_PASSWORD)
+headers = { "Content-Type": "application/yang-data+json", "Accept": "application/yang-data+json" }
+payload = { "Cisco-IOS-XE-native:logging": { "monitor": { "severity": "informational"  } } }
+r = requests.put(url, auth=auth, headers=headers, json=payload, verify=False)
+print(r.status_code, r.text)
