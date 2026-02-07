a979557f181cce6988ad85084687fcec8412d1ba
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2026-02-07 09:55:31 +0100

    Update netmiko_ssh_show_interface.py

--- netmiko/netmiko_ssh_show_interface.py
+++ netmiko/netmiko_ssh_show_interface.py
@@ -2,10 +2,10 @@ print("Connecting via SSH => show interface status (brief)")
 from netmiko import ConnectHandler
 sshCli = ConnectHandler(
     device_type="cisco_ios",
-    host="192.168.56.101",
+    host="192.168.56.xxx",
     port="22",
-    username="cisco",
-    password="cisco123!"
+    username="Get username",
+    password="Get password"
     )
 output=sshCli.send_command("show ip interface brief")
 print(output)
