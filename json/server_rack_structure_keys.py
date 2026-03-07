c4c3500e4555b5c21a698bbf0844cca143c30b07
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2026-03-07 10:22:32 +0100

    Create server_rack_structure_keys.py

---
+++ json/server_rack_structure_keys.py
@@ -0,0 +1,37 @@
+import json
+
+server_rack = {
+ "rack": [
+      { "server": { "dev_id": "S10" , "server_name": "svr1" , "domain": "biasc.be", "ip-address": "10.2.3.11" ,
+                     "os": "linux"  , "server_type": "vm" ,
+                     "services": [   
+                       {"service": "ad" , "service_type": "vm", "protocol": "tcp", "port": "389"},
+                       {"service": "dns", "service_type": "vm", "protocol": "udp", "port": "53"},
+                       {"service": "ntp", "service_type": "vm", "protocol": "udp", "port": "123"} 
+                    ]
+                  }
+      },
+      { "server": { "dev_id": "S20" , "server_name": "svr2" , "domain": "biasc.be", "ip-address": "10.2.3.12" ,
+                    "os": "linux"  , "server_type": "vm" ,
+                    "services": [   
+                      {"service": "flask", "service_type": "vm", "protocol": "tcp", "port": "8089"  }, 
+                      {"service": "db"   , "service_type": "vm", "protocol": "tcp", "port": "1521"  } 
+                    ]     
+                 }
+      },
+      { "server": { "dev_id": "S30" , "server_name": "svr3" ,  "domain": "biasc.be" , "ip-address": "10.2.3.13",
+                    "os": "linux"  , "server_type": "vm" ,
+                    "services": [   
+                      {"service": "dns" , "service_type": "vm", "protocol": "tcp", "port": "8089" }, 
+                      {"service": "ntp" , "service_type": "vm", "protocol": "udp", "port": "123" },
+                      {"service": "dhcp", "service_type": "docker", "protocol": "udp", "port": "67" }
+                    ] 
+                  }
+      }
+   ]
+}
+
+print(server_rack.keys())
+print(server_rack["rack"][0].keys())
+print(server_rack["rack"][0]["server"].keys())
+print(server_rack["rack"][0]["server"]["services"][0].keys())
