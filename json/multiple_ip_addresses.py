c919dc55cbf834ecd0d0dca66129a0c2b3ec513c
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2026-03-07 10:17:27 +0100

    Create multiple_ip_addresses.py

---
+++ json/multiple_ip_addresses.py
@@ -0,0 +1,13 @@
+multiple_addresses = [{"ip": "192.168.56.101", "netmask": "255.255.255.0"},
+                      {"ip": "192.0.2.1", "netmask": "255.255.255.252"} ]
+prefixes = { "255.255.255.0": "/24" , "255.255.255.192": "/26" , "255.255.255.252": "/30"}
+
+print(multiple_addresses)
+which_address = 0
+address = multiple_addresses[which_address]["ip"]
+print(address)
+mask = multiple_addresses[which_address]["netmask"]
+print(mask)
+prefix = prefixes[mask]
+print(prefix)
+print(address+prefix)
