acb417defb8c0cec4370ae63e22f1fc628874257
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2026-03-07 10:18:37 +0100

    Create hostnames.py

---
+++ json/hostnames.py
@@ -0,0 +1,5 @@
+import json
+multiple_hosts = [{"hostname" : "CSR1kv"}, {"hostname" : "CSR2kv"}, {"hostname" : "CSR3kv"}]
+print(multiple_hosts)
+first_host = multiple_hosts[0]["hostname"]
+print(first_host)
