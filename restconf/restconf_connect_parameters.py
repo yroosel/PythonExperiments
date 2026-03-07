29803dbec93b3869e1e49f984b755d221401a89b
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2026-03-07 16:39:52 +0100

    Update restconf_connect_parameters.py

--- restconf/restconf_connect_parameters.py
+++ restconf/restconf_connect_parameters.py
@@ -1,33 +1,14 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
-# Disable SSL warnings
 requests.packages.urllib3.disable_warnings()
-verify_ssl = False
 
-# Connection parameters
-CONNECTION_PARAMETERS = {
-    "HOST": "44.201.40.38",
-    "USER": "npe-restconf",
-    "PASS": "C1sco12345"
-}
+CONNECTION_PARAMETERS = {"HOST": "44.201.40.38", "USER": "npe-restconf", "PASS": "C1sco12345"}
 
-# RESTCONF URL
 url = f"https://{CONNECTION_PARAMETERS['HOST']}/restconf/"
+auth = HTTPBasicAuth(CONNECTION_PARAMETERS["USER"], CONNECTION_PARAMETERS["PASS"])
 
-# Headers
-headers = {
-    "Accept": "application/yang-data+json"
-}
+r = requests.get(url, headers={"Accept": "application/yang-data+json"}, auth=auth, verify=False)
 
-# Authentication
-auth = HTTPBasicAuth(
-    CONNECTION_PARAMETERS["USER"],
-    CONNECTION_PARAMETERS["PASS"]
-)
-
-# Send request
-response = requests.get(url, headers=headers, auth=auth, verify=verify_ssl)
-
-print(response.text)
-print(response.status_code)
+print(r.text)
+print(r.status_code)
