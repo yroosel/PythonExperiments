27eecc4ef2708172c04c55e2a3d2ef8d41aadd50
Yvan Rooseleer <33575242+yroosel@users.noreply.github.com>
2026-03-07 17:06:46 +0100

    Update restconf_hello.py

--- restconf/restconf_hello.py
+++ restconf/restconf_hello.py
@@ -3,7 +3,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 requests.packages.urllib3.disable_warnings()
 verify_ssl = False
-url = "https://192.168.56.xxx/restconf/"
+url = "https://x01.y01.z01.xxx/restconf/"
 headers = { "Accept": "application/yang-data+json"}
 auth = HTTPBasicAuth('Your User', 'Your Pass')
 response = requests.get(url, headers=headers, auth=auth, verify=verify_ssl)
